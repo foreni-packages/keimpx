7ff9da0541506519d468169e439c6fee88b2d188
Devon Kearns <dookie@kali.org>
2013-02-09 18:12:00 -0700

    Pre-build commit

--- debian/keimpx.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the keimpx package
-#
-0 4	* * *	root	[ -x /usr/bin/keimpx_maintenance ] && /usr/bin/keimpx_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(keimpx):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="keimpx" command="/usr/bin/keimpx"
